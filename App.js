69ba73dec84b606718f44b66ad46d4deab5ae076
LNarasimham <143478771+RLNarasimham@users.noreply.github.com>
2025-03-20 19:04:07 +0530

    Update App.js

--- App.js
+++ App.js
@@ -18,10 +18,10 @@ function App() {
           Learn React 
         </a>
           <center>
-        <a href="http://104.211.219.98/numbers/primes" target="_blank">Prime</a>
-        <a href="http://104.211.219.98/numbers/fibo" target="_blank">Fibo</a>
-        <a href="http://104.211.219.98/numbers/odd" target="_blank">Odd</a>
-        <a href="http://104.211.219.98/numbers/rand" target="_blank">Rand</a>
+        <a href="http://20.244.56.144/test/primes" target="_blank">Prime</a>
+        <a href="http://20.244.56.144/test/fibo" target="_blank">Fibo</a>
+        <a href="http://20.244.56.144/test/" target="_blank">Odd</a>
+        <a href="http://20.244.56.144/test/rand" target="_blank">Rand</a>
     </center>
       </header>
     </div>
